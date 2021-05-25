80011c29edd8ece667bb39f8218a135fcab48544
AladMocu <aa.molano@uniandes.edu.co>
2021-05-25 00:13:31 -0500

    Estructurada interfaz de usuario

--- interfaz.py
+++ interfaz.py
@@ -1,22 +1,23 @@
-import sys
-from PySide6 import QtWidgets
-from qt_material import apply_stylesheet
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtGui import QFontDatabase
+from qt_material import QtStyleTools
 
-# primer archivo
+
+########################################################################
+class RuntimeStylesheets(QMainWindow, QtStyleTools):
+    # ----------------------------------------------------------------------
+    def __init__(self):
+        """"""
+        super().__init__()
+        self.main = QUiLoader().load('window.ui', self)
+        self.apply_stylesheet(self.main, theme='dark_teal.xml')
+
 def draw():
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    # tamaño de la ventana
-    window.resize(700, 700)
-    window.show()
+    app = QApplication()
+    frame = RuntimeStylesheets()
+    frame.main.show()
     app.exec_()
-
-
-if __name__ == "__main__":
-    print("por favor ejecuta main.py")
-
-def no():
-    print('no')
 
 
 
