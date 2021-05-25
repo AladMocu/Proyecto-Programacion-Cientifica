c4c5072476c8fcf08eb17c7291948dcf28f0a330
AladMocu <aa.molano@uniandes.edu.co>
2021-05-24 20:12:42 -0500

    Cambie el tamaño de la ventana

--- interfaz.py
+++ interfaz.py
@@ -1,14 +1,15 @@
-import tkinter as tk
-import matplotlib
-
-# Creacion de la ventana
+import sys
+from PySide6 import QtWidgets
+from qt_material import apply_stylesheet
 
 
 def draw():
-    window = tk.Tk()
-    window.geometry('300x400')
-    window.title('Proyecto final')
-    window.mainloop()
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+    window.resize(700, 700)
+    window.show()
+    app.exec_()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print("por favor ejecuta main.py")
