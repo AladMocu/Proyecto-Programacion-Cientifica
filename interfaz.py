9ca236a0535ad81371bf09aa791f1bf1a43c3717
AladMocu <aa.molano@uniandes.edu.co>
2021-05-24 19:16:11 -0500

    Commit inicial

---
+++ interfaz.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import matplotlib
+
+# Creacion de la ventana
+
+
+def draw():
+    window = tk.Tk()
+    window.geometry('300x400')
+    window.title('Proyecto final')
+    window.mainloop()
+
+if __name__=="__main__":
+    print("por favor ejecuta main.py")
---
+++ main.py
@@ -0,0 +1,4 @@
+from interfaz import draw
+
+if __name__ == "__main__":
+    draw()
